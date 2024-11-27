a417865794d869d70ab5a246f8d429a6e01f8813
LaiThiHoa <laithihoa2005@gmail.com>
2024-11-27 10:30:11 +0700

    mupdate

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const loginButton = document.querySelector(".btn");
 loginButton.addEventListener("click", (event) => {
   event.preventDefault(); // Ngăn form gửi đi
-  window.location.href = "home.html"; // Chuyển hướng đến trang Register
+  window.location.href = "../home.html"; // Chuyển hướng đến trang Register
 });
 const sendResetMailButton = document.getElementById("sendmail");
 sendResetMailButton.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "sendmail.html";
+  window.location.href = "../sendmail.html";
 });
 const returnLogin = document.getElementById("returnlogin");
 returnLogin.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "home.html";
+  window.location.href = "../home.html";
 });
 const success = document.getElementById("success");
 success.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "home.html";
+  window.location.href = "../home.html";
 });
