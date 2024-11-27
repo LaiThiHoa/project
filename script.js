7fb0533009ee0ad4745c148d0a8951507b884a03
LaiThiHoa <laithihoa2005@gmail.com>
2024-11-27 09:53:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
+
+const menuBar = document.querySelector('.content nav .bx.bx-menu');
+const sideBar = document.querySelector('.sidebar');
+
+menuBar.addEventListener('click', () => {
+    sideBar.classList.toggle('close');
+});
+
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
+
+searchBtn.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        sideBar.classList.add('close');
+    } else {
+        sideBar.classList.remove('close');
+    }
+    if (window.innerWidth > 576) {
+        searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
+// 
+function showContent(contentId) {
+    // Ẩn tất cả các nội dung
+    const contents = document.querySelectorAll(".content");
+    contents.forEach((content) => {
+      content.style.display = "none";
+    });
+  
+    // Hiển thị nội dung được chọn
+    const selectedContent = document.getElementById(contentId);
+    if (selectedContent) {
+      selectedContent.style.display = "block";
+    }
+  }
+//   
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+console.log(header)
+const toTop = document.querySelector(".to-top");
+const heroSection = document.querySelector(".hero");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const heroSectionOffsetTop = heroSection.offsetTop;
+
+  if (scrollTop > heroSectionOffsetTop) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop;
+});
