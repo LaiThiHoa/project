7b9c7e835e2f46cf853531bdcd65585cab0ec45c
LaiThiHoa <laithihoa2005@gmail.com>
2024-11-27 14:29:49 +0700

    finallll

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 const loginButton = document.querySelector(".btn");
 loginButton.addEventListener("click", (event) => {
   event.preventDefault(); // Ngăn form gửi đi
-  window.location.href = "../home.html"; // Chuyển hướng đến trang Register
+  window.location.href = "../Bank/home.html"; // Chuyển hướng đến trang Register
 });
 const sendResetMailButton = document.getElementById("sendmail");
 sendResetMailButton.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "../login/sendmail.html";
+  window.location.href = "../sendmail.html";
 });
 const returnLogin = document.getElementById("returnlogin");
 returnLogin.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "../home.html";
+  window.location.href = "../Bank/home.html";
 });
 const success = document.getElementById("success");
 success.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "../home.html";
+  window.location.href = "../Bank/home.html";
+});
+const register = document.getElementById("register");
+register.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "../Bank/home.html";
 });
